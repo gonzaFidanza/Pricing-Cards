9208946fc0d7077926447dfc96c1d5bb729054ee
gonzaFidanza <g.fidanza.roch@gmail.com>
2025-07-22 20:13:28 -0300

    Added most popular

--- src/PricingSection.tsx
+++ src/PricingSection.tsx
@@ -6,7 +6,7 @@ import { pricingPlans } from './pricingData' ;
 function PricingSection() {
   return (
     <section className='bg-black w-full flex flex-col items-center p-8'>
-      <h2 className='text-white text-5xl font-bold my-10 text-center'>The <span className='bg-linear-to-r from-[#2B9AB5] to-[#01af49] text-transparent bg-clip-text'>Perfect Plan</span>  for Your Needs</h2>
+      <h2 className='text-white text-5xl font-bold my-12 text-center'>The <span className='bg-linear-to-r from-[#2B9AB5] to-[#01af49] text-transparent bg-clip-text'>Perfect Plan</span>  for Your Needs</h2>
       <div className='flex flex-col gap-8 mt-6 w-4/5 justify-around md:flex-row md:gap-12'>
         {
           pricingPlans.map(
--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -13,14 +13,14 @@ const PricingCard = (
 ) => {
   return (
     <article className = {`flex flex-col justify-between w-full max-w-sm rounded-xl p-8 bg-linear-to-b from-[#1f1f1f] to-[#151515]
-    ${highlight ? '' : ''}
+    ${highlight ? 'relative after:absolute after:content-["Most_Popular"] after:top-0 after:right-0 after:h-fit after:w-fit after:px-4 after:py-3 after:font-semibold after:text-[#1f1f1f] after:border-b-2 after:rounded-bl-2xl after:bg-linear-to-r after:from-[#2B9AB5] after:to-[#01af49]  md:scale-110' : ''}
     `}>
       <div>
-        <h2 className="text-white text-2xl font-semibold">{title}</h2>
-        <h3 className="text-[#676767]">{subtitle}</h3>
+        <h2 className={`text-2xl font-semibold ${highlight ? 'uppercase bg-linear-to-r from-[#2B9AB5] to-[#01af49] to-20% text-transparent bg-clip-text' : 'text-white'}`}>{title}</h2>
+        <h3 className="text-[#7f7f7f]">{subtitle}</h3>
         <div className="flex items-end gap-1 my-4">
           <p className="text-white text-4xl">${price}</p> 
-          <p className="text-[#676767]">/{billingCycle}</p>
+          <p className="text-[#7f7f7f]">/{billingCycle}</p>
         </div>
         <ul className="">
           {features.map(
@@ -31,8 +31,8 @@ const PricingCard = (
         </ul>
       </div>
       
-      <button className="bg-linear-to-r from-[#2B9AB5] to-[#01af49] items-center justify-center group relative flex text-white mt-10 cursor-pointer rounded-full p-[2.5px] font-semibold transition duration-300 ease-in-out hover:shadow-[0_0_4px_0.5px_#2B9AB5cc,0_0_12px_0.5px_#01af49cc]">
-          <div className="flex h-full w-full items-center rounded-full bg-[#151515] text-center font-medium transition ease-in-out hover:bg-[#1f1f1f]">
+      <button className="bg-linear-to-r from-[#2B9AB5] to-[#01af49] items-center justify-center group relative flex text-white mt-10 cursor-pointer rounded-full p-[2.5px] font-semibold transition duration-300 ease-linear ">
+          <div className="flex h-full w-full items-center rounded-full bg-linear-to-r from-[#151515] to-[#151515] text-center font-medium transition duration-500 ease-in-out hover:bg-linear-to-r hover:from-[#2B9AB5] hover:to-[#01af49]">
             <div className="w-full h-full py-3">Get Started</div>
           </div>
       </button>
